7527fe3d577bd87c99743c98d65cacb1670116c9
eunjin <huhej0315@gmail.com>
2024-01-15 15:36:23 +0900

    Initial commit!!

---
+++ src/main/java/com/example/webp/WebPApplication.java
@@ -0,0 +1,13 @@
+package com.example.webp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webp/WebPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
